a7594700752c005044d1c2fbefa04c8755a5b6ea
Chikondi Kamwendo <chikondikamwendo@yahoo.com>
2025-03-31 07:53:28 +0200

    Feat (NameStrategy): Can determine kit by name.

--- app/Strategies/NameStrategy.php
+++ app/Strategies/NameStrategy.php
@@ -9,12 +9,17 @@ use Closure;
 
 class NameStrategy implements Strategy
 {
+    /**
+     * Determines if the package is a kit based on its name.
+     */
     public function handle(Payload $payload, Closure $next): mixed
     {
         $packageName = $payload->package->name;
         $needles = ['starter-kit', 'starter kit', 'laravel starter kit'];
 
-        if (Str::contains($packageName, $needles, false)) {
+        $nameQualifies = Str::contains($packageName, $needles, false);
+
+        if ($nameQualifies && in_array('laravel', $payload->package->keywords)) {
             $payload->isKit = true;
             return null;
         }
