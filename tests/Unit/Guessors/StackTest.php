deb5e1b7919b88a7bd9ab62b6803043c84ea6eb5
Chikondi Kamwendo <chikondikamwendo@yahoo.com>
2025-04-02 12:06:26 +0200

    Refactor: Moved individual stack tests to one file.

---
+++ tests/Unit/Guessors/StackTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Guessors\Stack\React;
+use App\Guessors\Stack\Vue;
+use App\Guessors\Stack\Livewire;
+use App\ValueObjects\StackPayload;
+
+test('it guesses react stack', function () {
+    // Arrange
+    $payload = new StackPayload([
+        'npm' => ['react', 'react-dom', 'vue', 'livewire'],
+    ]);
+
+    // Act
+    (new React)->handle($payload, fn($payload) => null);
+
+    // Assert
+    expect($payload->getStacks())->toBe(['react']);
+});
+
+test('it guesses vue stack', function () {
+    // Arrange
+    $payload = new StackPayload([
+        'npm' => ['vue', 'vue-router', 'react', 'react-dom'],
+    ]);
+
+    // Act
+    (new Vue)->handle($payload, fn($payload) => null);
+
+    // Assert
+    expect($payload->getStacks())->toBe(['vue']);
+});
+
+test('it guesses livewire stack', function () {
+    // Arrange
+    $payload = new StackPayload([
+        'composer' => ['pestphp/pest', 'laravel/livewire', 'laravel/framework'],
+    ]);
+
+    // Act
+    (new Livewire)->handle($payload, fn($payload) => null);
+
+    // Assert
+    expect($payload->getStacks())->toBe(['laravel/livewire']);
+});
