f833face7bec5e3d2533103c726e4d0c94e02831
Chikondi Kamwendo <chikondikamwendo@yahoo.com>
2025-04-15 19:42:21 +0200

    Tests: Added docs page tests.

---
+++ tests/Feature/Http/DocsTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Inertia\Testing\AssertableInertia;
+
+it('returns a successful response', function () {
+    // Act
+    $response = $this->get('/docs');
+
+    // Assert
+    $response->assertStatus(200);
+    $response->assertInertia(function (AssertableInertia $page) {
+        $page->component('docs');
+    });
+});
