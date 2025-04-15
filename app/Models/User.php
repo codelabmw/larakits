9af05794e43098d6bb42c8506f472dc25095ea82
Chikondi Kamwendo <chikondikamwendo@yahoo.com>
2025-04-15 20:30:16 +0200

    Tests: Ignores code coverage for User model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+// @codeCoverageIgnoreStart
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
@@ -46,3 +47,5 @@ class User extends Authenticatable
         ];
     }
 }
+// @codeCoverageIgnoreEnd
+
