e25ae537121d46c974f057a19aa2ace86243950e
Chikondi Kamwendo <chikondikamwendo@yahoo.com>
2025-05-13 12:15:57 +0200

    Update (Kits): Ignores invalid sort parameters.

--- tests/Feature/Http/KitsTest.php
+++ tests/Feature/Http/KitsTest.php
@@ -127,3 +127,28 @@ it('can sort kits by stars', function (): void {
             ->where('kits.data.4.stars', 1);
     });
 });
+
+it('ignores invalid sort and order parameters', function (): void {
+    // Arrange
+    Kit::factory()->create(['downloads' => 1]);
+    Kit::factory()->create(['downloads' => 2]);
+    Kit::factory()->create(['downloads' => 3]);
+    Kit::factory()->create(['downloads' => 4]);
+    Kit::factory()->create(['downloads' => 5]);
+
+    // Act
+    $response = $this->get('/kits?sort=invalid&order=invalid');
+
+    // Assert
+    $response->assertStatus(200);
+    $response->assertInertia(function (AssertableInertia $page): void {
+        $page->component('landing/kits/index')
+            ->has('kits')
+            ->count('kits.data', 5)
+            ->where('kits.data.0.downloads', 5)
+            ->where('kits.data.1.downloads', 4)
+            ->where('kits.data.2.downloads', 3)
+            ->where('kits.data.3.downloads', 2)
+            ->where('kits.data.4.downloads', 1);
+    });
+});
