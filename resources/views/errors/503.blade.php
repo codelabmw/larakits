336e11ac01f248b97c4d36fa9d069b9faa69beca
Chikondi Kamwendo <chikondikamwendo@yahoo.com>
2025-08-09 18:15:55 +0200

    Update: Updated 503 page title.

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ config('app.name') }} - Maintenance Mode</title>
+    <title>{{ config('app.name') }} - Service Unavailable</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
